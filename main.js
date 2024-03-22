e39e1f67265f3409fe977f9510417e348dfb8d0a
Emilygabriela14 <104834076+Emilygabriela14@users.noreply.github.com>
2024-03-22 11:00:37 -0300

    .

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
+    
 
 
 
