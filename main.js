3a4a4fc6415e2b1c789154525c552ee7ddfb5422
Emilygabriela14 <104834076+Emilygabriela14@users.noreply.github.com>
2024-03-22 11:07:47 -0300

    .

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
