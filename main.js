13cf75a2331297510eca47a566009cf59fc02556
Emilygabriela14 <104834076+Emilygabriela14@users.noreply.github.com>
2024-04-05 09:51:42 -0300

    .

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
