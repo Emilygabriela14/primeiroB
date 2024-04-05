94ef58860f70e67a68b0385fd7f4d42c6c0913f0
Emilygabriela14 <104834076+Emilygabriela14@users.noreply.github.com>
2024-04-05 09:54:46 -0300

    .

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
-let tempoAtual = new Date();
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
